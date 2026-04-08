d38e6a84419c9400bf031a32851bba88a240ad4d
AGX <agxnh2020@gmail.com>
2026-04-08 19:05:15 +0200

    added the car document

---
+++ data/CarDocument.py
@@ -0,0 +1,7 @@
+class CarDocument:
+    def __init__(self, title, price, location, specs, link):
+        self.title = title
+        self.price = price
+        self.location = location
+        self.specs = specs
+        self.link = link
